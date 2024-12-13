3ef6ebf6facb8e4a101ddf86371c2ce05fb49d07
LiaoYijie <LiaoYijie@stu.xmu.edu.cn>
2024-12-13 18:57:33 +0800

    一些更改

--- src/colmap/estimators/coordinate_frame.h
+++ src/colmap/estimators/coordinate_frame.h
@@ -1,32 +1,4 @@
-// Copyright (c) 2023, ETH Zurich and UNC Chapel Hill.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//
-//     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
-//       its contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-
+//本文件中的内容与坐标系相关
 #pragma once
 
 #include "colmap/scene/reconstruction.h"
@@ -36,33 +8,33 @@
 
 namespace colmap {
 
+// 曼哈顿世界坐标系估计的选项结构体
+// 配置用于曼哈顿世界坐标系估计的参数。
+// 曼哈顿世界坐标系中：假设场景中的结构主要由正交的平面组成（如城市中的建筑物）
 struct ManhattanWorldFrameEstimationOptions {
-  // The maximum image size for line detection.
+  // 线段检测的最大图像尺寸。
   int max_image_size = 1024;
-  // The minimum length of line segments in pixels.
+  // 线段的最小长度（像素）。
   double min_line_length = 3;
-  // The tolerance for classifying lines into horizontal/vertical.
+  // 将线分类为水平/垂直的容差。
   double line_orientation_tolerance = 0.2;
-  // The maximum distance in pixels between lines and the vanishing points.
+  // 线与消失点之间的最大距离（像素）。
   double max_line_vp_distance = 0.5;
-  // The maximum cosine distance between estimated axes to be inliers.
+  // 估计轴之间作为内点的最大余弦距离。
   double max_axis_distance = 0.05;
 };
 
-// Estimate gravity vector by assuming gravity-aligned image orientation, i.e.
-// the majority of images is assumed to have the gravity vector aligned with an
-// upright image plane.
+// 一个函数
+// 通过图像的方向估计重力向量。
+// 图像的方向信息，输出：估计的重力向量，用于调整模型的方向
 Eigen::Vector3d EstimateGravityVectorFromImageOrientation(
     const Reconstruction& reconstruction, double max_axis_distance = 0.05);
 
-// Estimate the coordinate frame of the reconstruction assuming a Manhattan
-// world by finding the major vanishing points in each image. This function
-// assumes that the majority of images is taken in upright direction, i.e.
-// people are standing upright in the image. The orthonormal axes of the
-// estimated coordinate frame will be given in the columns of the returned
-// matrix. If one axis could not be determined, the respective column will be
-// zero. The axes are specified in the world coordinate system in the order
-// rightward, downward, forward.
+// 假设为曼哈顿世界，通过在每个图像中找到主要消失点来估计重建的坐标系。
+// 此函数假设大多数图像是直立拍摄的，即人们在图像中是直立的。
+// 估计坐标系的正交轴将作为返回矩阵的列给出。
+// 如果某个轴无法确定，相应的列将为零。
+// 这些轴在世界坐标系中按右、下、前的顺序指定。
 #ifdef COLMAP_LSD_ENABLED
 Eigen::Matrix3d EstimateManhattanWorldFrame(
     const ManhattanWorldFrameEstimationOptions& options,
@@ -70,16 +42,21 @@ Eigen::Matrix3d EstimateManhattanWorldFrame(
     const std::string& image_path);
 #endif
 
-// Aligns the reconstruction to the plane defined by running PCA on the 3D
-// points. The model centroid is at the origin of the new coordinate system
-// and the X axis is the first principal component with the Y axis being the
-// second principal component
+// 使用主成分分析将模型对齐到主要平面。
+//输入：模型的点云数据
+//输出：对齐后的模型数据，使其主要方向与坐标轴一致
 void AlignToPrincipalPlane(Reconstruction* recon, Sim3d* tform);
 
-// Aligns the reconstruction to the local ENU plane orientation. Rotates the
-// reconstruction such that the x-y plane aligns with the ENU tangent plane at
-// the point cloud centroid and translates the origin to the centroid.
-// If unscaled == true, then the original scale of the model remains unchanged.
+
+// 将模型对齐到东-北-上（ENU）平面
+//输入：模型的点云数据和可选的unscaled参数
+//输出：对齐后的模型数据，使其符合地理坐标系
+
+//ENU是一个地理坐标系，定义了东（E）、北（N）、上（U）的局部方向。
+//用于将3D模型与地理位置关联，特别是在导航和地理信息系统中
+
+// 旋转重建，使得x-y平面与点云质心处的ENU切平面对齐，并将原点平移到质心。
+// 如果unscaled == true，则模型的原始比例保持不变。
 void AlignToENUPlane(Reconstruction* recon, Sim3d* tform, bool unscaled);
 
 }  // namespace colmap
